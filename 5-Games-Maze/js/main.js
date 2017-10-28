b4bb309db7d10eb392341ca2183db579c2523ba4
zhenlychen <zhenlychen@foxmail.com>
2017-10-29 00:22:48 +0800

    Done Maze
    Maze is a good game!

---
+++ 5-Games-Maze/js/main.js
@@ -0,0 +1,63 @@
+"use strict";
+let gameStart = false;
+let goOut = false;
+
+function $(id) {
+  return document.getElementById(id);
+}
+
+function gameOver() {
+  gameStart = false;
+  $('gameBox').className = 'game-box';
+  $('tip').innerHTML = 'You Lose'
+  $('tip').className = 'tip tip-show';
+}
+
+function touchWall(e) {
+  if (gameStart) {
+    let oldClass = e.target.className;
+    gameOver();
+    e.target.className += ' red-wall';
+    setTimeout(function(e) {
+      e.target.className = oldClass;
+    }, 1000, e);
+  }
+}
+
+for (let wall of document.getElementsByName('walls')) {
+  wall.onmouseenter = touchWall;
+}
+
+// 开始游戏
+$('start').onmousemove = function(e) {
+  gameStart = true;
+  goOut = false;
+  $('tip').className = 'tip tip-hide';
+  $('gameBox').className = 'game-box pointer-hand';
+}
+
+$('end').onmousemove = function(e) {
+  if (gameStart) {
+    if (goOut) {
+      $('tip').innerHTML = 'Don\'t cheat, you should start from the \'S\' adn move to the \'E\' inside the maze!';
+    } else {
+      $('tip').innerHTML = 'You Win';
+    }
+    gameStart = false;
+    $('tip').className = 'tip tip-show';
+  }
+  $('gameBox').className = 'game-box';
+}
+
+$('gameBox').onmouseleave = function(e) {
+  if (gameStart) {
+    goOut = true;
+  }
+}
+
+$('gameBox').onmousedown = function(e) {
+  gameStart = false;
+  $('gameBox').className = 'game-box';
+  $('tip').innerHTML = 'You Can\'t Click!';
+  $('tip').className = 'tip tip-show';
+}
