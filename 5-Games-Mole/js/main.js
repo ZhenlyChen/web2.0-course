15764a106724cee225c19d57573ab3b0a687fe3a
zhenlychen <zhenlychen@foxmail.com>
2017-11-02 22:45:56 +0800

    :rocket: Use closure for mole

--- 5-Games-Mole/js/main.js
+++ 5-Games-Mole/js/main.js
@@ -1,90 +1,92 @@
 "use strict";
-let gameStart = false;
-let nowMole = -1;
-let time = 0;
-let score = 0;
 
 function $(id) {
   return document.getElementById(id);
 }
 
-for (let i = 0; i < 60; i++) {
-  let node = document.createElement('input');
-  node.type = 'radio';
-  node.value = i;
-  node.className = 'mole'
-  $('moles').appendChild(node);
-}
-
-Array.from(document.getElementsByClassName('mole'), radio => {
-  radio.checked = false;
-  radio.onclick = function(e) {
-    e.target.checked = false;
-    if (gameStart) hitMole(e.target.value);
+function moleCtrl() {
+  let score = 0;
+  let time = 0;
+  let gameStart = false;
+  let nowMole = -1;
+  let update = () => {
+    $('score').value = score;
+    $('time').value = time;
   };
-});
-
-$('startBtn').onclick = function(e) {
-  gameStart = !gameStart;
-  if (!gameStart) {
-    gameOver();
-  } else {
-    $('stateText').value = 'Playing';
-    time = 30;
-    score = 0;
-    makeMole();
-    update();
-    setTimeout(setTime, 1000);
-  }
-}
-
-function update() {
-  $('time').value = time;
-  $('score').value = score;
-}
-
-function makeMole() {
-  let id = Math.ceil(Math.random() * 60 - 1);
-  while (id === nowMole) {
-    id = Math.ceil(Math.random() * 60 - 1);
-  }
-  nowMole = id;
-  let radios = document.getElementsByClassName('mole');
-  radios[id].checked = true;
-}
-
-function hitMole(i) {
-  if (i == nowMole) {
-    score++;
-    makeMole();
-  } else {
-    score--;
-    if (score < 0) {
-      score = 0;
+  let makeMole = () => {
+    let id = Math.ceil(Math.random() * 60 - 1);
+    while (id === nowMole) {
+      id = Math.ceil(Math.random() * 60 - 1);
+    }
+    nowMole = id;
+    let radios = document.getElementsByClassName('mole');
+    radios[id].checked = true;
+  };
+  let gameOver = () => {
+    $('stateText').value = 'Game Over';
+    gameStart = false;
+    nowMole = -1;
+    Array.from(document.getElementsByClassName('mole'), e => {
+      e.checked = false;
+    });
+    alert('Game Over!\nYour score is ' + score);
+  };
+  let setTime = () => {
+    if (!gameStart) return;
+    if (time > 0) {
+      time--;
+      $('time').value = time;
+      setTimeout(setTime, 1000);
+    } else {
       gameOver();
-      return;
+    }
+  };
+  let control = {
+    hit: (i) => {
+      if (i == nowMole) {
+        score++;
+        makeMole();
+      } else {
+        score--;
+        if (score < 0) {
+          score = 0;
+          gameOver();
+          return;
+        }
+      }
+      update();
+    },
+    start: () => {
+      gameStart = !gameStart;
+      if (!gameStart) {
+        gameOver();
+      } else {
+        $('stateText').value = 'Playing';
+        time = 30;
+        score = 0;
+        makeMole();
+        update();
+        setTimeout(setTime, 1000);
+      }
     }
   }
-  update();
+  return control;
 }
 
-function gameOver() {
-  $('stateText').value = 'Game Over';
-  gameStart = false;
-  nowMole = -1;
-  Array.from(document.getElementsByClassName('mole'), e => {
-    e.checked = false;
-  });
-  alert('Game Over!\nYour score is ' + score);
-}
-
-function setTime() {
-  if (!gameStart) return;
-  if (time > 0) {
-    time--;
-    update();
-    setTimeout(setTime, 1000);
-  } else {
-    gameOver();
+(function() {
+  let Mole = moleCtrl();
+  let frag = document.createDocumentFragment();
+  for (let i = 0; i < 60; i++) {
+    let node = document.createElement('input');
+    node.type = 'radio';
+    node.value = i;
+    node.className = 'mole';
+    node.onclick = function(e) {
+      e.target.checked = false;
+      Mole.hit(e.target.value);
+    };
+    frag.appendChild(node);
   }
-}
+  $('moles').appendChild(frag);
+  $('startBtn').onclick = Mole.start;
+})();
